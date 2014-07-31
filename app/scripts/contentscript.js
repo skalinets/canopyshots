7d14d7e476c27ef566c4c4d2066c33708d07afd2
Serhiy Kalinets <kalinets@gmail.com>
2014-07-31 18:56:11 +0300

    adds logic for teamcity

--- app/scripts/contentscript.js
+++ app/scripts/contentscript.js
@@ -1,3 +1,37 @@
 'use strict';
 
-console.log('\'Allo \'Allo! Content script');
+window.addEventListener("load", function() {
+    var name = 'CanopyScreenShotter';
+    var app = angular.module(name, []);
+
+    var html = document.querySelector('html');
+    html.setAttribute('ng-app', '');
+    html.setAttribute('ng-csp', '');
+
+    var viewport = document.getElementById('tst_group_build_fail');
+    if (!viewport){
+        return;
+    }
+    viewport.setAttribute('ng-controller', 'MainController');
+    app.controller('MainController', function ($scope) {});
+
+    var e = angular.element(viewport.querySelector('.fullStacktrace'));
+    console.log('found collection:');
+    console.log(e);
+
+    e.append("<div my-directive></div>");
+
+//    var myDirective = document.createElement('div');
+//    myDirective.setAttribute('my-directive', '');
+//    viewport.appendChild(myDirective);
+
+    app.directive('myDirective', function() {
+        return {
+            restrict: 'EA',
+            replace: true,
+            template: '<button>hello!</button>'
+        };
+    });
+
+    angular.bootstrap(html, [name], []);
+});
