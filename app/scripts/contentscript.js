5a83956463b9305dc2abdf094285923e783bb430
Serhiy Kalinets <kalinets@gmail.com>
2014-08-11 17:56:35 +0300

    fixes shwing screenshots for multiple errors

--- app/scripts/contentscript.js
+++ app/scripts/contentscript.js
@@ -13,23 +13,40 @@ window.addEventListener("load", function() {
         return;
     }
     viewport.setAttribute('ng-controller', 'MainController');
-    app.controller('MainController', function ($scope) {});
+    app.controller('MainController', function ($scope, $compile) {
+        $scope.refresh = function() {
+            console.log('I am here');
+            var e = angular.element(viewport.querySelectorAll('.fullStacktrace'));
 
-    var e = angular.element(viewport.querySelector('.fullStacktrace'));
-    console.log('found collection:');
-    console.log(e);
+            angular.forEach(e, function(e1){
 
-    e.append("<div my-directive></div>");
+                var element = angular.element(e1);
 
-//    var myDirective = document.createElement('div');
-//    myDirective.setAttribute('my-directive', '');
-//    viewport.appendChild(myDirective);
+                var match = element.text().match(/canopy-image\(([^)]+)\)/);
+                if (match) {
+                    var text = match[1];
+                    element.text(element.text().replace(/canopy-image\(([^)]+)\)/, ""));
+                    var a = '<button ng-click="showImage(\'' + text + '\')">Click me!</button>';
+                    element.prepend("<img class='tmp' src='data:image/gif;base64," + text + "' /><br><br><br>");
+                }
+
+            });
+        };
+
+        $scope.showImage = function(s) {
+            console.log("I am about to show image");
+            console.log(s);
+        }
+    });
+
+    var myDirective = document.createElement('my-directive');
+    viewport.appendChild(myDirective);
 
     app.directive('myDirective', function() {
         return {
             restrict: 'EA',
             replace: true,
-            template: '<button>hello!</button>'
+            template: '<button ng-click="refresh()">Show Screenshots for Open Stacktraces</button>'
         };
     });
 
